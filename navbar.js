b46aaea4543187d2a3e2a72b20d34c49945dbeb5
Paarth Rathod <paarth0809@gmail.com>
2024-06-20 22:31:55 -0400

    Initial commit

---
+++ navbar.js
@@ -0,0 +1,11 @@
+
+// top nav open menu when clicked on hamburger icon
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
